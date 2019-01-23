37184a1df50eb240bb04a21b1a92655f711e6387
kathireshcks <36156226+kathireshcks@users.noreply.github.com>
2019-01-23 23:23:38 +0530

    Create substring.c
    a 
    b
    c
    ab
    bc
    ca
    abc

---
+++ logic/substring.c
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+  
+// Function to print all sub strings 
+void subString(char str[], int n)  
+{ 
+    // Pick starting point 
+    for (int len = 1; len <= n; len++)  
+    {     
+        // Pick ending point 
+        for (int i = 0; i <= n - len; i++)  
+        { 
+            //  Print characters from current 
+            // starting point to current ending 
+            // point.   
+            int j = i + len - 1;             
+            for (int k = i; k <= j; k++)  
+                cout << str[k]; 
+              
+            cout << endl; 
+        } 
+    } 
+} 
+  
+// Driver program to test above function 
+int main()  
+{ 
+    char str[] = "abc"; 
+    subString(str, strlen(str)); 
+    return 0; 
+} 
